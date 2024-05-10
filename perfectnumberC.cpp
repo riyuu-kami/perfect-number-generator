4f43a2ddd43199a6c71ecc2ab3b3d55c89b7c528
riyuu-kami <157930699+riyuu-kami@users.noreply.github.com>
2024-05-11 00:48:12 +0200

    Add files via upload

---
+++ perfectnumberC.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+bool isPerfect(int n) {
+    int sum = 1;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            sum += i;
+            if (i != n / i)
+                sum += n / i;
+        }
+    }
+    return sum == n && n != 1;
+}
+
+int main() {
+    for (int num = 1000000; num <= 100000000; num++) {
+        if (isPerfect(num)) {
+            cout << num << endl;
+        }
+    }
+    return 0;
+}
